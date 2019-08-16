2b2e4396d389a84664ef8e3a7cd225a9ac353239
Lindongjie <547022637@qq.com>
2019-08-16 15:09:57 +0800

    'update'

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -26,7 +26,7 @@ class IndexController extends Controller
     public function actionGetCase(){
         Yii::$app->response->format = Response::FORMAT_JSON;
         $cid = Yii::$app->request->get('cid',false);
-        $limit = Yii::$app->request->get('limit',10);
+        $limit = Yii::$app->request->get('limit',5);
         $query = Project::find()->orderBy(['created_at' => SORT_DESC])->limit($limit);
         if($cid){
             $query->andWhere(['=','cid',$cid]);
@@ -51,7 +51,7 @@ class IndexController extends Controller
         $cid = Yii::$app->request->get('cid');
         $project_category = ProjectCategory::find()->all();
 
-        $query = Project::find()->orderBy(['created_at' => SORT_DESC])->limit(5);
+        $query = Project::find()->orderBy(['created_at' => SORT_DESC]);
         if($cid){
             $query->andWhere(['=','cid',$cid]);
         };
