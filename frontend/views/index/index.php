84904a3a3cb21fbc0cc73556317f5ef9ae45f0c8
林东杰 <lanny@lindongjiedeMacBook-Pro.local>
2019-08-17 16:55:18 +0800

    update

--- frontend/views/index/index.php
+++ frontend/views/index/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use frontend\assets\AppAsset;
-AppAsset::addJs($this, 'js/index.js?v=1');
+AppAsset::addJs($this, 'js/index.js?v=2');
 $this->title = "首页";
 ?>
 
@@ -15,176 +15,176 @@ $this->title = "首页";
 
 <!--第一屏 -->
 <div id="main">
-<!--    <div class="section section1">-->
-<!--        <div class="content">-->
-<!--            <div class="check-img bg"></div>-->
-<!--            <div class="check-img icon"></div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--    <!--第二屏 案例-->-->
-<!--    <div class="section section2">-->
-<!--        <div class="wapper">-->
-<!--            <div class="title fl">-->
-<!--                <p style="font-size: 18px">设计案例</p>-->
-<!--                <p style="font-size: 12px;color: #999">Design case</p>-->
-<!--            </div>-->
-<!--            <nav class="nav fr">-->
-<!--                <a class="tab-nav" href="javascript:void(0)">全部</a>-->
-<!--                --><?php //foreach ($project_category as $k => $v): ?>
-<!--                    <a class="tab-nav" href="javascript:void(0)" data-id="--><?//=$v['id']?><!--">--><?//=$v['name']?><!--</a>-->
-<!--                --><?php //endforeach; ?>
-<!--            </nav>-->
-<!--            <div class="line"></div>-->
-<!--        </div>-->
-<!--        <div class="pic">-->
-<!--            <div class="tab-content">-->
-<!--                <div class="pic-wapper">-->
-<!---->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!---->
-<!---->
-<!--        <div class="m-more">-->
-<!--            <a href="--><?//=\yii\helpers\Url::to(['index/case-list'])?><!--" style="color: #333333;font-size: 14px;">MORE</a>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--    <!--第三屏 关于-->-->
-<!--    <div class="section section3">-->
-<!--        <div class="about">-->
-<!--            <div class="check-img com fl"></div>-->
-<!--            <div class="content fl">-->
-<!--                <div class="cell">-->
-<!--                    <h3>关于</h3>-->
-<!--                    <p style="font-size: 12px;color:#999;font-weight: normal;margin-top: -10px">ABOUT US</p>-->
-<!--                    <P style="font-size: 14px;color:#666;font-weight: normal;margin-top: 60px;line-height: 30px">-->
-<!--                        --><?//=$about ?? '资料整理中...'?>
-<!--                    </P>-->
-<!--                    <div class="a-more">-->
-<!--                        <a href="#" style="color: #333333;font-size: 14px;margin-top: 200px;">MORE</a>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!--    <!--第四屏 服务-->-->
-<!--    <div class="section section4">-->
-<!--        <div class="box">-->
-<!--            <div class="w1175">-->
-<!--                <div class="top clearfix">-->
-<!--                    <div class="title fl">-->
-<!--                        <h3>服务领域</h3>-->
-<!--                        <p>Service area</p>-->
-<!--                    </div>-->
-<!--                    <div class="nav fr">-->
-<!--                        <img src="/img/index_home4_nav.png">-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="content clearfix">-->
-<!--                    <div class="pic">-->
-<!--                        <img src="/img/index_home4_web.png">-->
-<!--                        <img src="/img/index_case4.jpg">-->
-<!--                        <img src="/img/index_case3.jpg">-->
-<!--                        <img src="/img/index_home4_web.png">-->
-<!--                    </div>-->
-<!--                    <div class="info-list-wapper">-->
-<!--                        <ul id="info-list" class="info-list">-->
-<!--                            <li>-->
-<!--                                <div class="info">-->
-<!--                                    <h3>网站设计/Web design</h3>-->
-<!--                                    <p>网站是企业在互联网领域的形象代言，企业通过网站展示企业形象，-->
-<!--                                        宣传企业产品与服务，通过网站获得用户反馈。我们可以为您提供量-->
-<!--                                        身定制企业网站服务，并可以根据互联网的不断发展，提供持续的技-->
-<!--                                        术支持，让您的品牌在互联网领域扩展延伸。</p>-->
-<!--                                </div>-->
-<!--                            </li>-->
-<!--                            <li>-->
-<!--                                <div class="info">-->
-<!--                                    <h3>网站设计/Web design2222</h3>-->
-<!--                                    <p>网站是企业在互联网领域的形象代言，企业通过网站展示企业形象，-->
-<!--                                        宣传企业产品与服务，通过网站获得用户反馈。我们可以为您提供量-->
-<!--                                        身定制企业网站服务，并可以根据互联网的不断发展，提供持续的技-->
-<!--                                        术支持，让您的品牌在互联网领域扩展延伸。</p>-->
-<!--                                </div>-->
-<!--                            </li>-->
-<!--                            <li>-->
-<!--                                <div class="info">-->
-<!--                                    <h3>网站设计/Web design33333</h3>-->
-<!--                                    <p>网站是企业在互联网领域的形象代言，企业通过网站展示企业形象，-->
-<!--                                        宣传企业产品与服务，通过网站获得用户反馈。我们可以为您提供量-->
-<!--                                        身定制企业网站服务，并可以根据互联网的不断发展，提供持续的技-->
-<!--                                        术支持，让您的品牌在互联网领域扩展延伸。</p>-->
-<!--                                </div>-->
-<!--                            </li>-->
-<!--                        </ul>-->
-<!--                        <div class="btn_l fl">-->
-<!--                            <i class="iconfont icon-arrowleft"></i>-->
-<!--                        </div>-->
-<!--                        <div class="btn_r fl">-->
-<!--                            <i class="iconfont icon-arrowright"></i>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!---->
-<!---->
-<!--    <!--第五屏 文章-->-->
-<!--    <div class="section section5">-->
-<!--        <div class="wapper">-->
-<!--            <div class="title fl">-->
-<!--                <p style="font-size: 18px">精选文章</p>-->
-<!--                <p style="font-size: 12px;color: #999">Article</p>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="time">-->
-<!--            <a href="javascript:void(0)">5-16</a>-->
-<!--            <a href="javascript:void(0)">5-17</a>-->
-<!--            <a href="javascript:void(0)">5-18</a>-->
-<!--        </div>-->
-<!--        <div class="line"></div>-->
-<!--        <div class="tab_content">-->
-<!--            <div class="content1">-->
-<!--                <div class="box">-->
-<!--                    <div class="box-cont">-->
-<!--                        <img src="/img/index_novel1.png" alt="">-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="info">-->
-<!--                    <h2 class="title">交互设计师应该具备哪些思维方式</h2>-->
-<!--                    <p class="desc">交互设计</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="content1">-->
-<!--                <div class="box">-->
-<!--                    <div class="box-cont">-->
-<!--                        <img src="/img/index_novel2.png" alt="">-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="info">-->
-<!--                    <h2 class="title">交互设计师应该具备哪些思维方式</h2>-->
-<!--                    <p class="desc">交互设计</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="content1">-->
-<!--                <div class="box">-->
-<!--                    <div class="box-cont">-->
-<!--                        <img src="/img/index_novel3.png" alt="">-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="info">-->
-<!--                    <h2 class="title">交互设计师应该具备哪些思维方式</h2>-->
-<!--                    <p class="desc">交互设计</p>-->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--        <div class="m-more">-->
-<!--            <a href="#" style="color: #333333;font-size: 14px;">MORE</a>-->
-<!--        </div>-->
-<!--    </div>-->
+    <div class="section section1">
+        <div class="content">
+            <div class="check-img bg"></div>
+            <div class="check-img icon"></div>
+        </div>
+    </div>
+    <!--第二屏 案例-->
+    <div class="section section2">
+        <div class="wapper">
+            <div class="title fl">
+                <p style="font-size: 18px">设计案例</p>
+                <p style="font-size: 12px;color: #999">Design case</p>
+            </div>
+            <nav class="nav fr">
+                <a class="tab-nav" href="javascript:void(0)">全部</a>
+                <?php foreach ($project_category as $k => $v): ?>
+                    <a class="tab-nav" href="javascript:void(0)" data-id="<?=$v['id']?>"><?=$v['name']?></a>
+                <?php endforeach; ?>
+            </nav>
+            <div class="line"></div>
+        </div>
+        <div class="pic">
+            <div class="tab-content">
+                <div class="pic-wapper">
+
+                </div>
+            </div>
+        </div>
+
+
+        <div class="m-more">
+            <a href="<?=\yii\helpers\Url::to(['index/case-list'])?>" style="color: #333333;font-size: 14px;">MORE</a>
+        </div>
+    </div>
+
+    <!--第三屏 关于-->
+    <div class="section section3">
+        <div class="about">
+            <div class="check-img com fl"></div>
+            <div class="content fl">
+                <div class="cell">
+                    <h3>关于</h3>
+                    <p style="font-size: 12px;color:#999;font-weight: normal;margin-top: -10px">ABOUT US</p>
+                    <P style="font-size: 14px;color:#666;font-weight: normal;margin-top: 60px;line-height: 30px">
+                        <?=$about ?? '资料整理中...'?>
+                    </P>
+                    <div class="a-more">
+                        <a href="#" style="color: #333333;font-size: 14px;margin-top: 200px;">MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--第四屏 服务-->
+    <div class="section section4">
+        <div class="box">
+            <div class="w1175">
+                <div class="top clearfix">
+                    <div class="title fl">
+                        <h3>服务领域</h3>
+                        <p>Service area</p>
+                    </div>
+                    <div class="nav fr">
+                        <img src="/img/index_home4_nav.png">
+                    </div>
+                </div>
+                <div class="content clearfix">
+                    <div class="pic">
+                        <img src="/img/index_home4_web.png">
+                        <img src="/img/index_case4.jpg">
+                        <img src="/img/index_case3.jpg">
+                        <img src="/img/index_home4_web.png">
+                    </div>
+                    <div class="info-list-wapper">
+                        <ul id="info-list" class="info-list">
+                            <li>
+                                <div class="info">
+                                    <h3>网站设计/Web design</h3>
+                                    <p>网站是企业在互联网领域的形象代言，企业通过网站展示企业形象，
+                                        宣传企业产品与服务，通过网站获得用户反馈。我们可以为您提供量
+                                        身定制企业网站服务，并可以根据互联网的不断发展，提供持续的技
+                                        术支持，让您的品牌在互联网领域扩展延伸。</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="info">
+                                    <h3>网站设计/Web design2222</h3>
+                                    <p>网站是企业在互联网领域的形象代言，企业通过网站展示企业形象，
+                                        宣传企业产品与服务，通过网站获得用户反馈。我们可以为您提供量
+                                        身定制企业网站服务，并可以根据互联网的不断发展，提供持续的技
+                                        术支持，让您的品牌在互联网领域扩展延伸。</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="info">
+                                    <h3>网站设计/Web design33333</h3>
+                                    <p>网站是企业在互联网领域的形象代言，企业通过网站展示企业形象，
+                                        宣传企业产品与服务，通过网站获得用户反馈。我们可以为您提供量
+                                        身定制企业网站服务，并可以根据互联网的不断发展，提供持续的技
+                                        术支持，让您的品牌在互联网领域扩展延伸。</p>
+                                </div>
+                            </li>
+                        </ul>
+                        <div class="btn_l fl">
+                            <i class="iconfont icon-arrowleft"></i>
+                        </div>
+                        <div class="btn_r fl">
+                            <i class="iconfont icon-arrowright"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!--第五屏 文章-->
+    <div class="section section5">
+        <div class="wapper">
+            <div class="title fl">
+                <p style="font-size: 18px">精选文章</p>
+                <p style="font-size: 12px;color: #999">Article</p>
+            </div>
+        </div>
+        <div class="time">
+            <a href="javascript:void(0)">5-16</a>
+            <a href="javascript:void(0)">5-17</a>
+            <a href="javascript:void(0)">5-18</a>
+        </div>
+        <div class="line"></div>
+        <div class="tab_content">
+            <div class="content1">
+                <div class="box">
+                    <div class="box-cont">
+                        <img src="/img/index_novel1.png" alt="">
+                    </div>
+                </div>
+                <div class="info">
+                    <h2 class="title">交互设计师应该具备哪些思维方式</h2>
+                    <p class="desc">交互设计</p>
+                </div>
+            </div>
+            <div class="content1">
+                <div class="box">
+                    <div class="box-cont">
+                        <img src="/img/index_novel2.png" alt="">
+                    </div>
+                </div>
+                <div class="info">
+                    <h2 class="title">交互设计师应该具备哪些思维方式</h2>
+                    <p class="desc">交互设计</p>
+                </div>
+            </div>
+            <div class="content1">
+                <div class="box">
+                    <div class="box-cont">
+                        <img src="/img/index_novel3.png" alt="">
+                    </div>
+                </div>
+                <div class="info">
+                    <h2 class="title">交互设计师应该具备哪些思维方式</h2>
+                    <p class="desc">交互设计</p>
+                </div>
+            </div>
+        </div>
+        <div class="m-more">
+            <a href="#" style="color: #333333;font-size: 14px;">MORE</a>
+        </div>
+    </div>
 
 
     <!--第六屏 联系 -->
